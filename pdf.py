591cf0bfd4aeae8683821fb1620208994d19486d
Jyothi <bjyothipriya.999@gmail.com>
2023-05-11 11:06:23 +0200

    updated csv file

--- pdf.py
+++ pdf.py
@@ -2,23 +2,27 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-url = "https://link.springer.com/chapter/10.1007/978-3-031-09917-5_32"
+url = "https://link.springer.com/chapter/10.1007/978-3-031-09917-5_32#author-information"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
-
 
 tag = soup.find("title")
 
 print(tag.string)
 
-# authors = soup.find_all("a")[0]
-# desired_author = []
+# authors = soup.find_all("p")
+# author_names = []
 # print(find (authors.link))
 # for author in authors:
-  #  if author.string == "Petri Vuorimaa":
-   #     desired_author.append(authors.string)
-#        print(desired_author)
+# if author.string == "Qinqin Wang":
+# author_names.append(authors.text)
+# print(authors)
+
+author_name = soup.find('p', {'class': 'c-article-author-affiliation__authors-list'})
+author_text = author_name.text.strip()
+
+print("authors_name : " + author_text)
 
 keywords = soup.find_all("span")
 desired_keywords = []
@@ -33,10 +37,10 @@ for keyword in keywords:
 
 print(desired_keywords)
 
-with open('output.csv', mode='a', newline='') as file:
-       writer = csv.writer(file)
-      # writer.writerow(['Title', 'Desired Keywords'])
-       writer.writerow([tag.string, ', '.join(desired_keywords)])
+with open('data.csv', mode='a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Title', 'Desired Keywords', 'Authors'])
+    writer.writerow([tag.string, ', '.join(desired_keywords), ',' .join(author_name)])
 
 # tags = soup.find_all("a")
 # print("title: " + tags.get('a'))
