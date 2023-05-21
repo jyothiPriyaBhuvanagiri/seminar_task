388da0234166bfec54e7ad244aa0349c9d215e86
Jyothi <bjyothipriya.999@gmail.com>
2023-05-21 17:02:27 +0200

    updated code

--- pdf.py
+++ pdf.py
@@ -11,14 +11,6 @@ tag = soup.find("title")
 
 print(tag.string)
 
-# authors = soup.find_all("p")
-# author_names = []
-# print(find (authors.link))
-# for author in authors:
-# if author.string == "Qinqin Wang":
-# author_names.append(authors.text)
-# print(authors)
-
 author_name = soup.find('p', {'class': 'c-article-author-affiliation__authors-list'})
 author_text = author_name.text.strip()
 
@@ -47,15 +39,12 @@ abstract_div = soup.find("div", {"id": "Abs1-content"})
 abstract_text = abstract_div.text.strip()
 print(abstract_text)
 
+links = soup.find('span', {'class': 'c-bibliographic-information__value'})
+links_text = links.text.strip()
+print(links_text)
+
 with open('data.csv', mode='a', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Title' , 'Desired Keywords' , 'Authors', 'Affiliation', 'Abstract'])
-    writer.writerow([tag.string, ', '.join(desired_keywords), ',' .join(author_name), (affiliation_text) , (abstract_text), ])
+    writer.writerow([tag.string, ', '.join(desired_keywords), ',' .join(author_name), (affiliation_text) , (abstract_text) ])
 
-# tags = soup.find_all("a")
-# print("title: " + tags.get('a'))
-
-
-# for item in soup.select('a'):
-     # print(item.prettify())
-    # Z
