65fa2371ca420287750323cff4b04c46041a30a0
Jyothi <bjyothipriya.999@gmail.com>
2023-05-22 20:54:02 +0200

    updated code

---
+++ fetch_links.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+url = 'https://link.springer.com/book/10.1007/978-3-319-91662-0'
+
--- pdf.py
+++ pdf.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-url = "https://link.springer.com/chapter/10.1007/978-3-031-09917-5_1"
+url = "https://link.springer.com/chapter/10.1007/978-3-319-91662-0_2"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
@@ -11,20 +11,22 @@ tag = soup.find("title")
 
 print(tag.string)
 
+# authors of the paper
 author_name = soup.find('p', {'class': 'c-article-author-affiliation__authors-list'})
 author_text = author_name.text.strip()
 
 print("authors_name : " + author_text)
 
-keywords = soup.find_all("span")
-desired_keywords = []
-
+# affiliations of the paper
 affiliation = soup.find("p", {'class': 'c-article-author-affiliation__address'})
 affiliation_text = affiliation.text.strip()
 affiliation.extract()
 
 print(affiliation_text)
 
+# keywords of the paper
+keywords = soup.find_all("span")
+desired_keywords = []
 for keyword in keywords:
     if keyword.string == "Recommendation":
         desired_keywords.append(keyword.string)
@@ -35,16 +37,25 @@ for keyword in keywords:
 
 print(desired_keywords)
 
+# abstract related to the paper
 abstract_div = soup.find("div", {"id": "Abs1-content"})
 abstract_text = abstract_div.text.strip()
 print(abstract_text)
 
+# link related to the paper
 links = soup.find('span', {'class': 'c-bibliographic-information__value'})
-links_text = links.text.strip()
-print(links_text)
+link_of_paper = links.text.strip()
+print(link_of_paper)
 
+# year of the paper
+year_of_paper = soup.find("p", {'class': 'c-chapter-book-details right-arrow'})
+year = year_of_paper.text.strip()
+
+print(year)
+
+# csv of the data
 with open('data.csv', mode='a', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Title' , 'Desired Keywords' , 'Authors', 'Affiliation', 'Abstract'])
-    writer.writerow([tag.string, ', '.join(desired_keywords), ',' .join(author_name), (affiliation_text) , (abstract_text) ])
+    writer.writerow([tag.string, ', '.join(desired_keywords), ',' .join(author_name), (affiliation_text) , (abstract_text), link_of_paper, year])
 
